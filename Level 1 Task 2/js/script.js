7bfdd6aab55423124cf6726ffabf5b447ede18b5
ABDUL SAMAD KHAN <104279302+samadkhan1387@users.noreply.github.com>
2023-11-07 00:12:03 +0500

    Add files via upload

---
+++ Level 1 Task 2/js/script.js
@@ -0,0 +1,38 @@
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+/*========== sticky navbar ==========*/
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top>= offset && top <offset+ height) {
+           navlinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+           });
+        };
+    });
+ let header = document.querySelector('.header');
+ header.classList.toggle('sticky', window.scrollY > 100);
+};
+
+/*========== swiper ==========*/
+var swiper = new Swiper(".mySwiper",{
+    slidesPerview: 1,
+    spaceBetween: 50,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
